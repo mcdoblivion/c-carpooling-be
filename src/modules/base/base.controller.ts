800d28eff7d955b9b7b10ce844c71905c29926a2
Cuong Dong Minh <mcd.oblivion@gmail.com>
2022-10-03 01:06:07 +0700

    Add subscriber to automatically create wallet after creating user

--- src/modules/base/base.controller.ts
+++ src/modules/base/base.controller.ts
@@ -3,21 +3,21 @@ import { UserEntity } from 'src/typeorm/entities'
 import { SearchResult } from 'src/types'
 
 export interface BaseController<T> {
-  search(searchDto: SearchDto, searchBy?: UserEntity): Promise<SearchResult<T>>
+  search?(searchDto: SearchDto, searchBy?: UserEntity): Promise<SearchResult<T>>
 
-  getAll(getBy?: UserEntity): Promise<T[]>
+  getAll?(getBy?: UserEntity): Promise<T[]>
 
-  getOneById(id: number, getBy?: UserEntity): Promise<T>
+  getOneById?(id: number, getBy?: UserEntity): Promise<T>
 
-  create(createDto: Record<string, any>, createBy?: UserEntity): Promise<T>
+  create?(createDto: Record<string, any>, createBy?: UserEntity): Promise<T>
 
-  updateOneById(
+  updateOneById?(
     id: number,
     updateDto: Record<string, any>,
     updateBy?: UserEntity,
   ): Promise<T>
 
-  deleteOneById(id: number, deletedBy?: UserEntity): Promise<any>
+  deleteOneById?(id: number, deletedBy?: UserEntity): Promise<any>
 
-  deleteMany({ IDs }: { IDs: number[] }, deletedBy?: UserEntity): Promise<any>
+  deleteMany?({ IDs }: { IDs: number[] }, deletedBy?: UserEntity): Promise<any>
 }
